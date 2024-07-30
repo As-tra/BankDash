372bfbf86a2eed91778f4751a0d095e2d8fbece3
Assil <astradhdbjd@gmail.com>
2024-07-30 11:23:20 +0100

    add app styles

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const kInter = 'Inter';
+const kLato = 'Lato';
+const kPrimary1 = Color(0xff718EBF);
+const kPrimary2 = Color(0xffB1B1B1);
+const kPrimary3 = Color(0xff232323);
+const kSurface = Color(0xffFFFFFF);
+const kSecondary = Color(0xff1814F3);
+
+const kDarkBlue = LinearGradient(
+  colors: [
+    Color(0xff123288),
+    Color(0xff295EEC),
+  ],
+);
---
+++ lib/utils/styles.dart
@@ -0,0 +1,128 @@
+import 'package:bank_dashboard/constants.dart';
+import 'package:bank_dashboard/utils/size_config.dart';
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static TextStyle interRegular12(BuildContext context) {
+    return TextStyle(
+      fontFamily: kInter,
+      fontSize: getResponsiveFontSize(context: context, baseSize: 12),
+      color: kPrimary1,
+    );
+  }
+
+  static TextStyle interMedium16(BuildContext context) {
+    return TextStyle(
+      fontFamily: kInter,
+      fontSize: getResponsiveFontSize(context: context, baseSize: 16),
+      fontWeight: FontWeight.w500,
+      color: kPrimary2,
+    );
+  }
+
+  static TextStyle interRegular16(BuildContext context) {
+    return TextStyle(
+      fontFamily: kInter,
+      fontSize: getResponsiveFontSize(context: context, baseSize: 16),
+      color: kPrimary3,
+    );
+  }
+
+  static TextStyle interMedium12(BuildContext context) {
+    return TextStyle(
+      fontFamily: kInter,
+      fontSize: getResponsiveFontSize(context: context, baseSize: 12),
+      fontWeight: FontWeight.w500,
+      color: kPrimary1,
+    );
+  }
+
+  static TextStyle interRegular13(BuildContext context) {
+    return TextStyle(
+      fontFamily: kInter,
+      fontSize: getResponsiveFontSize(context: context, baseSize: 13),
+      color: kPrimary1,
+    );
+  }
+
+  static TextStyle interRegular15(BuildContext context) {
+    return TextStyle(
+      fontFamily: kInter,
+      fontSize: getResponsiveFontSize(context: context, baseSize: 15),
+      color: kPrimary1,
+    );
+  }
+
+  static TextStyle interMedium18(BuildContext context) {
+    return TextStyle(
+      fontFamily: kInter,
+      fontSize: getResponsiveFontSize(context: context, baseSize: 18),
+      fontWeight: FontWeight.w500,
+      color: kPrimary2,
+    );
+  }
+
+  static TextStyle interMedium13(BuildContext context) {
+    return TextStyle(
+      fontFamily: kInter,
+      fontSize: getResponsiveFontSize(context: context, baseSize: 13),
+      fontWeight: FontWeight.w500,
+      color: kPrimary3,
+    );
+  }
+
+  static TextStyle interMedium14(BuildContext context) {
+    return TextStyle(
+      fontFamily: kInter,
+      fontSize: getResponsiveFontSize(context: context, baseSize: 14),
+      fontWeight: FontWeight.w500,
+      color: kPrimary3,
+    );
+  }
+
+  static TextStyle interMedium15(BuildContext context) {
+    return TextStyle(
+      fontFamily: kInter,
+      fontSize: getResponsiveFontSize(context: context, baseSize: 15),
+      fontWeight: FontWeight.w500,
+      color: kSecondary,
+    );
+  }
+
+  static TextStyle interSemiBold16(BuildContext context) {
+    return TextStyle(
+      fontFamily: kInter,
+      fontSize: getResponsiveFontSize(context: context, baseSize: 16),
+      fontWeight: FontWeight.w600,
+      color: kPrimary3,
+    );
+  }
+  static TextStyle latoSemiBold16(BuildContext context) {
+    return TextStyle(
+      fontFamily: kLato,
+      fontSize: getResponsiveFontSize(context: context, baseSize: 15),
+      fontWeight: FontWeight.w600,
+      color: kSurface,
+    );
+  }
+}
+
+double getResponsiveFontSize({
+  required BuildContext context,
+  required double baseSize,
+}) {
+  double scaleFactor = getScaleFactor(context);
+  double responsiveFontSize = baseSize * scaleFactor;
+  return responsiveFontSize.clamp(baseSize * 0.8, baseSize * 1.2);
+}
+
+double getScaleFactor(BuildContext context) {
+  double width = MediaQuery.sizeOf(context).width;
+  if (width > SizeConfig.desktop) {
+    return width / 1400;
+  } else if (width > SizeConfig.tablet) {
+    return width / 812;
+  } else {
+    return width / 400;
+  }
+}
